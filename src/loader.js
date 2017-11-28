9eb9ea475e0a27327320ad6160e694b64daa61dd
Leigh Simpson <code@simpleigh.com>
2017-11-28 22:47:41 +0000

    Style tweaks: two spaces for indentation, fat arrow export

--- src/loader.js
+++ src/loader.js
@@ -3,42 +3,42 @@ import { getOptions } from 'loader-utils';
 import validateOptions from 'schema-utils';
 
 const schema = {
-    type: 'object',
-    properties: {
-        varname: {
-            type: 'string'
-        },
-        strip: {
-            type: 'boolean'
-        },
-        append: {
-            type: 'boolean'
-        }
+  type: 'object',
+  properties: {
+    varname: {
+      type: 'string'
+    },
+    strip: {
+      type: 'boolean'
+    },
+    append: {
+      type: 'boolean'
     }
+  }
 }
 
-export default function (source) {
-    const options = getOptions(this);
-    validateOptions(schema, options, 'dotjs-loader');
+export default source => {
+  const options = getOptions(this);
+  validateOptions(schema, options, 'dotjs-loader');
 
-    [
-        'evaluate',
-        'interpolate',
-        'encode',
-        'use',
-        'define',
-        'conditional',
-        'iterate',
-        'varname',
-        'strip',
-        'append',
-    ].forEach(option => {
-        if (options[option]) {
-            doT.templateSettings[option] = options[option];
-        }
-    });
+  [
+    'evaluate',
+    'interpolate',
+    'encode',
+    'use',
+    'define',
+    'conditional',
+    'iterate',
+    'varname',
+    'strip',
+    'append',
+  ].forEach(option => {
+    if (options[option]) {
+      doT.templateSettings[option] = options[option];
+    }
+  });
 
-    doT.templateSettings.selfcontained = true;
+  doT.templateSettings.selfcontained = true;
 
-    return 'export default ' + doT.template(source);
+  return 'export default ' + doT.template(source);
 };
